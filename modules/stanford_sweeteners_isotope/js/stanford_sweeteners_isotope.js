0fed3acf5890e752258ede33aa941d46ded7cfc3
Shea McKinney <sherakama@gmail.com>
2015-02-26 17:16:11 -0800

    CAW-131 CAW-356: create deep linking first pass

--- modules/stanford_sweeteners_isotope/js/stanford_sweeteners_isotope.js
+++ modules/stanford_sweeteners_isotope/js/stanford_sweeteners_isotope.js
@@ -3,6 +3,7 @@
   Drupal.behaviors.stanford_sweeteners_isotope = {
     attach: function (context, settings) {
       Stanford_sweeteners_isotope.init(context, settings);
+      Stanford_sweeteners_isotope.historystate_init(context, settings);
     }
   };
 
@@ -43,7 +44,6 @@ Stanford_sweeteners_isotope.init = function(context, settings) {
     });
 
     var options = {};
-    console.log(filters);
 
     // If there are no filters or all filters are selected show everything.
     if (filters.length >= 0 && filters.length < $optionBoxes.length) {
@@ -58,10 +58,90 @@ Stanford_sweeteners_isotope.init = function(context, settings) {
 
 };
 
+/**
+ * [isotope_update description]
+ * @return {[type]} [description]
+ */
+Stanford_sweeteners_isotope.isotope_update = function() {
+
+  var $container = $('#isotope-container');
+
+  var $optionSets = $('#isotope-checkbox-options .option-set'),
+      $optionBoxes = $optionSets.find('input');
+
+  var filters = [];
+  $optionBoxes.filter(":checked").each(function(i, v) {
+    filters.push($(v).val());
+  });
+
+  var options = {};
+
+  // If there are no filters or all filters are selected show everything.
+  if (filters.length >= 0 && filters.length < $optionBoxes.length) {
+    options.filter = filters.join(", ");
+  }
+  else {
+    options.filter = "*";
+  }
+
+  $container.isotope(options);
+};
 
 
+/**
+ * Adds deeplinking to the filters
+ * @param  {[type]} context  [description]
+ * @param  {[type]} settings [description]
+ * @return {[type]}          [description]
+ */
+Stanford_sweeteners_isotope.historystate_init = function(context, settings) {
 
+  // Bind to StateChange Event
+  // History.Adapter.bind(window,'statechange',function() {
+  //   var State = History.getState();
+  // });
 
+  // Cuz we roll like that.
+  $ = jQuery;
+
+  // Get elements
+  var $optionSets = $('#isotope-checkbox-options .option-set', context),
+      $optionBoxes = $optionSets.find('input');
+
+  // Handle the current state
+  var state = History.getState();
+  if (state.hash !== Drupal.settings.basePath + "sweeteners") {
+    var hashparams = state.hash.replace(Drupal.settings.basePath + "sweeteners", "");
+    // var selectors = hashparams.split("/");
+    var selector = hashparams.replace(/\//g, ",#");
+    selector = selector.replace(",", "");
+    $optionBoxes.not(selector).attr("checked", false);
+    Stanford_sweeteners_isotope.isotope_update();
+  }
+
+  // Add change handlers to the checkboxes.
+  $optionBoxes.change(function(e) {
+    var filters = [];
+
+    $optionBoxes.filter(":checked").each(function(i, v) {
+      filters.push($(v).val());
+    });
+
+    var hash = Drupal.settings.basePath + "sweeteners";
+
+    // If there are no filters or all filters are selected show everything.
+    if (filters.length > 0 && filters.length < $optionBoxes.length) {
+      var params = filters.join("/");
+      params = params.replace(/\./g, "");
+      hash += "/" + params;
+    }
+
+    // Making changes!
+    History.replaceState(null, null, hash);
+
+  });
+
+};
 
 // var filters = {};
 
