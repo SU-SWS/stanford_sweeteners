1ecf44a6ebe263d86c8d9dad99bd3d529f5ed107
Shea McKinney <sherakama@gmail.com>
2015-05-22 14:28:13 -0700

    CAW-482: Rebuild pager and nav display

--- js/stanford_sweeteners.slick.js
+++ js/stanford_sweeteners.slick.js
@@ -11,20 +11,29 @@
         $("." + id + " .view-content", context).slick(settings);
       });
 
+      Drupal.behaviors.stanford_sweeteners.button_count = 0;
       this.move_buttons();
 
-      // Fire on resize.
+      // Fire on change to dots.
       $('.sweetener-arrows').bind('DOMSubtreeModified', function(e) {
-        Drupal.behaviors.stanford_sweeteners.move_buttons();
+        var count = $(this).find("li").length;
+        if (count !== Drupal.behaviors.stanford_sweeteners.button_count) {
+          Drupal.behaviors.stanford_sweeteners.move_buttons();
+          Drupal.behaviors.stanford_sweeteners.button_count = count;
+        }
       });
 
     },
+
     move_buttons: function () {
       var dots = $(".slick-dots");
       var next = $(".sweetener-arrows").find(".slick-next");
       if (dots.length) {
         next.remove();
         next.insertAfter(dots);
+        next.click(function() {
+          $(this).parent().parent().find(".slick-slider").slick("slickNext");
+        });
       }
     }
   };
