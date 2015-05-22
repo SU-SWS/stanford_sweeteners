fd03d9d9c2b9f9ac22e57475038a7ac5346f84b9
Shea McKinney <sherakama@gmail.com>
2015-05-22 13:59:46 -0700

    CAW-482: Pager styles and moveing

--- js/stanford_sweeteners.slick.js
+++ js/stanford_sweeteners.slick.js
@@ -6,10 +6,29 @@
 
   Drupal.behaviors.stanford_sweeteners = {
     attach: function (context, settings) {
+
       $.each(Drupal.settings.stanford_sweeteners, function(id, settings) {
         $("." + id + " .view-content", context).slick(settings);
       });
+
+      this.move_buttons();
+
+      // Fire on resize.
+      $('.sweetener-arrows').bind('DOMSubtreeModified', function(e) {
+        Drupal.behaviors.stanford_sweeteners.move_buttons();
+      });
+
+    },
+    move_buttons: function () {
+      var dots = $(".slick-dots");
+      var next = $(".sweetener-arrows").find(".slick-next");
+      if (dots.length) {
+        next.remove();
+        next.insertAfter(dots);
+      }
     }
   };
 
+
 })(jQuery);
+
